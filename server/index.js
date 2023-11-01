0198def2546afa0c17fcea348fdf704106f5801e
Codeworm08 <siddhantmajumder@gmail.com>
2023-11-01 21:12:32 +0530

    required path module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 const app = express();
 let Parser = require('rss-parser');
 let parser = new Parser();
